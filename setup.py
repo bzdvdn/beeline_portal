7ab875bfcafccd9a52b3f6e929e659e76def076f
bzdvdn <bzdv.dn@gmail.com>
2024-08-22 12:34:14 +0300

    update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(path):
 
 setup(
     name="beeline-portal",
-    version='0.0.2',
+    version="0.0.3",
     packages=find_packages(exclude=("tests", "docs", "examples", "venv")),
     install_requires=["requests", "pytz"],
     description="Beeline cloudpbx portal api wrapper",
